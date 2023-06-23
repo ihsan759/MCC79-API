09152a8ad14b761ad82c4683514597f921fbab0e
ihsan759 <mihsandf759@gmail.com>
2023-06-23 09:45:57 +0700

    Create Interface Repository

---
+++ API/Contracts/IRoomRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+
+namespace API.Contracts
+{
+    public interface IRoomRepository
+    {
+        ICollection<Room> GetAll();
+        Room? GetByGuid(Guid guid);
+        Room Create(Room university);
+        bool Update(Room university);
+        bool Delete(Guid guid);
+    }
+}
