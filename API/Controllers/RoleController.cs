1ddd6a1ed1c94a0449fc7fbfc98f560f17eea898
ihsan759 <mihsandf759@gmail.com>
2023-06-23 09:46:28 +0700

    Create Controller

---
+++ API/Controllers/RoleController.cs
@@ -0,0 +1,73 @@
+﻿using API.Contracts;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("Api/roles")]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleRepository _repository;
+
+        public RoleController(IRoleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var role = _repository.GetAll();
+            if (!role.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(role);
+        }
+
+        [HttpGet("{guid}")]
+        public IActionResult GetByGuid(Guid guid)
+        {
+            var role = _repository.GetByGuid(guid);
+            if (role is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(role);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Role role)
+        {
+            var isCreated = _repository.Create(role);
+            return Ok(isCreated);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Role role)
+        {
+            var isUpdated = _repository.Update(role);
+            if (!isUpdated)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Guid guid)
+        {
+            var isDeleted = _repository.Delete(guid);
+            if (!isDeleted)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+    }
+}
