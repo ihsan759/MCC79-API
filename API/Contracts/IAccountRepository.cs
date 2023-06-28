7c724477bc01f0fbf5ac947440d8158b5583f607
ihsan759 <mihsandf759@gmail.com>
2023-06-28 14:55:47 +0700

    Add Check OTP (demo)

--- API/Contracts/IAccountRepository.cs
+++ API/Contracts/IAccountRepository.cs
@@ -4,5 +4,6 @@ namespace API.Contracts
 {
     public interface IAccountRepository : IGeneralRepository<Account>
     {
+        Account? CheckOtp(string email, int otp);
     }
 }
--- API/Repositories/AccountRepository.cs
+++ API/Repositories/AccountRepository.cs
@@ -7,5 +7,22 @@ namespace API.Repositories
     public class AccountRepository : GeneralRepository<Account>, IAccountRepository
     {
         public AccountRepository(BookingDbContext context) : base(context) { }
+
+        public Account? CheckOtp(string email, int otp)
+        {
+            return _context.Set<Account>().Join(_context.Set<Employee>(),
+                a => a.Guid, e => e.Guid, (a, e) =>
+                new { Account = a, Employee = e }).FirstOrDefault(e => e.Employee.Email == email && e.Account.Otp == otp)?.Account;
+
+            /*var employee = _context.Set<Employee>().FirstOrDefault(e => e.Email == email);
+
+            if (employee == null)
+            {
+                return null;
+            }
+
+            return _context.Set<Account>().FirstOrDefault(a => a.Guid == employee.Guid && a.Otp == otp);*/
+
+        }
     }
 }
