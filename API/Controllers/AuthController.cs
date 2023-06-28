ed2dbea9f24cf6d56fdfba484ea1124ad6ab8f87
ihsan759 <mihsandf759@gmail.com>
2023-06-28 10:43:18 +0700

    Add Auth Register

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using API.DTOs.Auth;
+using API.DTOs.Bookings;
+using API.Services;
+using API.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("Api/Auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public ActionResult Register(RegisterDto registerDto)
+        {
+            var createRegister = _authService.Register(registerDto);
+            if (createRegister is null)
+            {
+                return BadRequest(new ResponseHandlers<GetBookingDto>
+                {
+                    Code = StatusCodes.Status400BadRequest,
+                    Status = HttpStatusCode.BadRequest.ToString(),
+                    Message = "Data not created"
+                });
+            }
+
+            return Ok(new ResponseHandlers<RegisterDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Successfully created",
+                Data = createRegister
+            });
+        }
+    }
+}
