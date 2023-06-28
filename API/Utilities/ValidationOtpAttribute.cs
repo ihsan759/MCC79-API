f890937101306154f5fda6ca93999e97f12d90fb
ihsan759 <mihsandf759@gmail.com>
2023-06-28 14:54:16 +0700

    Create ValidationOtpAttribute.cs

---
+++ API/Utilities/ValidationOtpAttribute.cs
@@ -0,0 +1,36 @@
+﻿using API.Contracts;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Utilities
+{
+
+    // Not used
+    public class ValidationOtpAttribute : ValidationAttribute
+    {
+        private readonly string _email;
+        public ValidationOtpAttribute(string email)
+        {
+            _email = email;
+        }
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is null) return ValidationResult.Success;
+
+            var employeeRepository = (IAccountRepository)validationContext.GetService(typeof(IAccountRepository))!;
+            var entity = employeeRepository.CheckOtp(_email, int.Parse(value.ToString()));
+            if (entity is null)
+            {
+                return new ValidationResult("Otp doesn't match");
+            }
+            if (entity.IsUsed == true)
+            {
+                return new ValidationResult("Otp has been used");
+            }
+            if (entity.ExpiredTime > DateTime.Now)
+            {
+                return new ValidationResult("Otp alredy expired");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
